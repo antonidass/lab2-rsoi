5e75531f4b2225b56048bc0c9a6687c71b65630f
antonidass <akrikoff@gmail.com>
2023-11-12 19:25:58 +0300

    фикс ci/cd

--- postgres/30-init-db.sh
+++ postgres/30-init-db.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-psql "user=postgres hostaddr=127.0.0.1 port=5431 password=postgres" < scripts/db-v3.sql
 psql "user=postgres hostaddr=127.0.0.1 port=5431 password=postgres" < cars/create.sql
 psql "user=postgres hostaddr=127.0.0.1 port=5431 password=postgres" < payment/create.sql
 psql "user=postgres hostaddr=127.0.0.1 port=5431 password=postgres" < rental/create.sql
